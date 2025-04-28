1b7095c9df3c0bb9f1495a0f167b155b40978cab
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-04-28 15:14:21 +0300

    Hidden Search Widget exercise. Make a button slide to the side and stretch an input field and make it visible and focused.

---
+++ Hidden Search Widget/script.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector("#expandBtn");
+const searchInput = document.querySelector("#search");
+
+// slide button on X axis
+// we want the button to slide to the edge of the input
+// need to know the width of the input when expanded.
+// take half of the input size (if 200/2)
+// then move the btn by the difference of its width
+// (sliderWidth/2)-(btnWidth/2)
+// use that result to translate the button
+//(80px in this case)
+btn.addEventListener("click", () => {
+	searchInput.classList.toggle("expand");
+	btn.classList.toggle("slideX");
+
+	if (searchInput.classList.contains("expand")) {
+		searchInput.focus();
+	}
+});
