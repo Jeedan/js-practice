0449cd113eeb9f830d0d412dd24304bdee71b528
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-04-30 20:41:28 +0300

    added check to see if boxes exist

--- Scroll Animation/script.js
+++ Scroll Animation/script.js
@@ -2,10 +2,21 @@ const boxSlide = document.querySelectorAll(".box:nth-of-type(n+4)");
 const mainBox = document.querySelector(".box");
 const body = document.querySelector("body");
 
-let triggerBox = window.innerHeight - boxSlide[0].offsetHeight;
+let triggerBox = 0;
+const buffer = 2;
+
+const setTriggerBox = () => {
+	if (boxSlide.length > 0) {
+		triggerBox = window.innerHeight - boxSlide[0].offsetHeight;
+	} else {
+		triggerBox = window.innerHeight;
+	}
+};
+
+setTriggerBox();
 
 window.addEventListener("resize", () => {
-	triggerBox = window.innerHeight - boxSlide[0].offsetHeight;
+	setTriggerBox();
 });
 
 let lastRun = 0;
@@ -24,9 +35,15 @@ const slideBoxes = () => {
 		boxSlide.forEach((b, i) => {
 			const boxTop = b.getBoundingClientRect().top;
 
-			if (boxTop < triggerBox && !b.classList.contains("slide")) {
+			if (
+				boxTop < triggerBox - buffer &&
+				!b.classList.contains("slide")
+			) {
 				b.classList.add("slide");
-			} else if (boxTop > triggerBox && b.classList.contains("slide")) {
+			} else if (
+				boxTop > triggerBox + buffer &&
+				b.classList.contains("slide")
+			) {
 				b.classList.remove("slide");
 			}
 		});
