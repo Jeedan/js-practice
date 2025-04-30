4d49a5253bbb46ea40ba98cd45fa71bffde0abf9
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-04-30 16:57:15 +0300

    removed comments

--- Scroll Animation/script.js
+++ Scroll Animation/script.js
@@ -2,19 +2,12 @@ const boxSlide = document.querySelectorAll(".box:nth-of-type(n+4)");
 const mainBox = document.querySelector(".box");
 const body = document.querySelector("body");
 
-const triggerBox = window.innerHeight - boxSlide[0].offsetHeight;
-
-// boxSlide.forEach((b, i) => {
-// 	if (i % 2 === 0) {
-// 		b.classList.add("slide-left");
-// 	} else {
-// 		b.classList.add("slide-right");
-// 	}
-// });
+let triggerBox = window.innerHeight - boxSlide[0].offsetHeight;
 
 window.addEventListener("resize", () => {
-	const triggerBox = window.innerHeight - boxSlide[0].offsetHeight;
+	triggerBox = window.innerHeight - boxSlide[0].offsetHeight;
 });
+
 window.addEventListener("scroll", () => {
 	boxSlide.forEach((b) => {
 		const boxTop = b.getBoundingClientRect().top;
