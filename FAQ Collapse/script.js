aa6338a23d4dec97935ef863d557094c8d56ebb1
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-05-12 00:56:07 +0300

    FAQ Collapse my janky version

---
+++ FAQ Collapse/script.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector(".btn");
+const btns = document.querySelectorAll(".btn");
+const faqContainers = document.querySelectorAll(".faq-container");
+const faqOpens = document.querySelectorAll("#open");
+const faqCloses = document.querySelectorAll("#close");
+const collapseContainers = document.querySelectorAll("#collapse-container");
+
+btns.forEach((b, idx) => {
+	b.addEventListener("click", () => {
+		faqContainers[idx].classList.toggle("collapse");
+		collapseContainers[idx].classList.toggle("collapse");
+		faqOpens[idx].classList.toggle("collapse");
+		faqCloses[idx].classList.toggle("collapse");
+	});
+});
