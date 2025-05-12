ad5bd561d4f9b3f0a1b30ab57cfca8830f4e1f0c
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-05-12 15:47:06 +0300

    cleaned up my code using the course

--- FAQ Collapse/script.js
+++ FAQ Collapse/script.js
@@ -1,14 +1,11 @@
-const btns = document.querySelectorAll(".btn");
-const faqContainers = document.querySelectorAll(".faq-container");
-const faqOpens = document.querySelectorAll("#open");
-const faqCloses = document.querySelectorAll("#close");
-const collapseContainers = document.querySelectorAll("#collapse-container");
+const btns = document.querySelectorAll(".faq-toggle");
+const faqContainers = document.querySelectorAll(".faq");
+
+const faq = document.querySelector(".faq");
+const toggle = document.querySelector(".faq-toggle");
 
 btns.forEach((b, idx) => {
 	b.addEventListener("click", () => {
 		faqContainers[idx].classList.toggle("collapse");
-		collapseContainers[idx].classList.toggle("collapse");
-		faqOpens[idx].classList.toggle("collapse");
-		faqCloses[idx].classList.toggle("collapse");
 	});
 });
